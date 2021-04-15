764142616a092802ba868b58778891acfd979b80
jimin <w2037@e-mirim.hs.kr>
2021-04-15 19:17:54 +0900

    직접풀어보기 4-4 시작함 스위치로 변경

--- app/src/main/java/ks/hs/emirim/wlalsdl_04/project4_2/MainActivity.java
+++ app/src/main/java/ks/hs/emirim/wlalsdl_04/project4_2/MainActivity.java
@@ -5,37 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-    CheckBox checkStart;
+    Switch switchStart;
     RadioGroup rg;
     ImageView imgv;
     LinearLayout linear;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        checkStart = findViewById(R.id.checkbox_start);
+        switchStart = findViewById(R.id.switch_start);
         linear = findViewById(R.id.linear);
         rg = findViewById(R.id.rg);
         imgv = findViewById(R.id.imgv);
         Button btnDone = findViewById(R.id.btn_done);
 
-        checkStart.setOnCheckedChangeListener(checkListener);
+        switchStart.setOnCheckedChangeListener(checkListener);
         btnDone.setOnClickListener(btnListener);
+
     }
     CompoundButton.OnCheckedChangeListener checkListener = new CompoundButton.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-            if (isChecked){
+            if(isChecked){
                 linear.setVisibility(View.VISIBLE);
             }else{
                 linear.setVisibility(View.INVISIBLE);
@@ -49,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.radio_dog:
                     imgv.setImageResource(R.drawable.bibi);
                     break;
-                    case R.id.radio_cat:
+                case R.id.radio_cat:
                     imgv.setImageResource(R.drawable.lulu);
                     break;
                 case R.id.radio_rabbit:
